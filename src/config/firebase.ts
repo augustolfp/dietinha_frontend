d7e15fc5e05de299dfe9701b49659900f9f8487c
augustolfp <augustolfp@gmail.com>
2023-06-20 16:11:37 -0300

    feat(routes): implement app first pages

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,5 +1,12 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import {
+    getAuth,
+    createUserWithEmailAndPassword,
+    updateProfile,
+    onAuthStateChanged,
+    signInWithEmailAndPassword,
+    signOut,
+} from "firebase/auth";
 
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -15,4 +22,11 @@ const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
 
-export default auth;
+export {
+    auth,
+    createUserWithEmailAndPassword,
+    updateProfile,
+    onAuthStateChanged,
+    signInWithEmailAndPassword,
+    signOut,
+};
