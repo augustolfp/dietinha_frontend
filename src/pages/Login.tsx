fd15e9c8caa674df9b0affb84dae28df64528018
augustolfp <augustolfp@gmail.com>
2023-07-07 20:33:21 -0300

    build(tailwind): configure tailwind css

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -17,34 +17,36 @@ export default function Login() {
     };
 
     return (
-        <>
-            <h1>Login</h1>
-            <form onSubmit={handleLogin}>
-                Email:
-                <br />
-                <input
-                    type="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    disabled={isLoading}
-                />
-                <br />
-                Password:
-                <br />
-                <input
-                    type="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    disabled={isLoading}
-                />
-                <br />
-                <button type="submit" disabled={isLoading}>
-                    {isLoading ? "loading..." : "Log In"}
+        <div className="flex flex-col items-center bg-slate-100">
+            <div className=" bg-white rounded-xl shadow-lg">
+                <h1>Login</h1>
+                <form onSubmit={handleLogin}>
+                    Email:
+                    <br />
+                    <input
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        disabled={isLoading}
+                    />
+                    <br />
+                    Password:
+                    <br />
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        disabled={isLoading}
+                    />
+                    <br />
+                    <button type="submit" disabled={isLoading}>
+                        {isLoading ? "loading..." : "Log In"}
+                    </button>
+                </form>
+                <button disabled={isLoading} onClick={handleGoogleLogin}>
+                    Sign in with google!
                 </button>
-            </form>
-            <button disabled={isLoading} onClick={handleGoogleLogin}>
-                Sign in with google!
-            </button>
-        </>
+            </div>
+        </div>
     );
 }
