4e640352ce7bf93ece21f9e0123e9fe385741f0d
augustolfp <augustolfp@gmail.com>
2023-07-14 20:45:40 -0300

    refactor(login): move password input to dedicated component

--- src/pages/Login/LoginForm.tsx
+++ src/pages/Login/LoginForm.tsx
@@ -1,10 +1,9 @@
-import { useForm, SubmitHandler } from "react-hook-form";
+import { useForm, SubmitHandler, FormProvider } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useState, SyntheticEvent } from "react";
 import { DotWave } from "@uiball/loaders";
-import { BiSolidShow, BiSolidHide } from "react-icons/bi";
 import useSignIn from "../../hooks/authHooks/useSignIn";
 import { loginFormSchema } from "../../schemas/credentialsSchemas";
+import PasswordInput from "../../components/PasswordInput";
 
 type Inputs = {
     email: string;
@@ -13,14 +12,17 @@ type Inputs = {
 
 export default function LoginForm() {
     const { isLoading, signIn } = useSignIn();
-    const [passwordVisible, setPasswordVisible] = useState(false);
+
+    const createUserForm = useForm<Inputs>({
+        resolver: zodResolver(loginFormSchema),
+    });
 
     const {
         register,
         handleSubmit,
         setError,
         formState: { errors },
-    } = useForm<Inputs>({ resolver: zodResolver(loginFormSchema) });
+    } = createUserForm;
 
     const onSubmit: SubmitHandler<Inputs> = async ({ email, password }) => {
         try {
@@ -33,66 +35,54 @@ export default function LoginForm() {
         }
     };
 
-    const togglePasswordVisibility = (e: SyntheticEvent) => {
-        e.preventDefault();
-        setPasswordVisible(!passwordVisible);
-    };
-
     return (
-        <form className="flex flex-col gap-3" onSubmit={handleSubmit(onSubmit)}>
-            <div className="flex flex-col gap-1">
-                <label htmlFor="email">E-mail</label>
-                <input
-                    id="email"
-                    type="text"
-                    {...register("email")}
-                    aria-invalid={errors.email ? "true" : "false"}
-                    disabled={isLoading}
-                />
-                {errors.email && (
-                    <p role="alert" className="text-xs text-red-700">
-                        {errors.email.message}
-                    </p>
-                )}
-            </div>
-            <div className="flex flex-col gap-1">
-                <label htmlFor="password">Senha</label>
-                <div className="flex items-center relative">
+        <FormProvider {...createUserForm}>
+            <form
+                className="flex flex-col gap-3"
+                onSubmit={handleSubmit(onSubmit)}
+            >
+                <div className="flex flex-col gap-1">
+                    <label htmlFor="email">E-mail</label>
                     <input
-                        className="w-full"
-                        id="password"
-                        type={passwordVisible ? "text" : "password"}
-                        {...register("password")}
+                        id="email"
+                        type="text"
+                        {...register("email")}
+                        aria-invalid={errors.email ? "true" : "false"}
+                        disabled={isLoading}
+                    />
+                    {errors.email && (
+                        <p role="alert" className="text-xs text-red-700">
+                            {errors.email.message}
+                        </p>
+                    )}
+                </div>
+                <div className="flex flex-col gap-1">
+                    <label htmlFor="password">Senha</label>
+                    <PasswordInput
                         aria-invalid={errors.password ? "true" : "false"}
                         disabled={isLoading}
                     />
-                    <div
-                        className="absolute right-2"
-                        onClick={togglePasswordVisibility}
-                    >
-                        {passwordVisible ? <BiSolidHide /> : <BiSolidShow />}
-                    </div>
+
+                    {errors.password && (
+                        <p role="alert" className="text-xs text-red-700">
+                            {errors.password.message}
+                        </p>
+                    )}
                 </div>
 
-                {errors.password && (
+                <button
+                    type="submit"
+                    className="p-2 bg-pink-500 flex items-center justify-center"
+                    disabled={isLoading}
+                >
+                    {isLoading ? <DotWave /> : "Entrar"}
+                </button>
+                {errors.root && (
                     <p role="alert" className="text-xs text-red-700">
-                        {errors.password.message}
+                        {errors.root.serverError.message}
                     </p>
                 )}
-            </div>
-
-            <button
-                type="submit"
-                className="p-2 bg-pink-500 flex items-center justify-center"
-                disabled={isLoading}
-            >
-                {isLoading ? <DotWave /> : "Entrar"}
-            </button>
-            {errors.root && (
-                <p role="alert" className="text-xs text-red-700">
-                    {errors.root.serverError.message}
-                </p>
-            )}
-        </form>
+            </form>
+        </FormProvider>
     );
 }
