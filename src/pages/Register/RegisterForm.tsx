23993cee886bf1be46ada11fde250087dc916c0f
augustolfp <augustolfp@gmail.com>
2023-07-15 00:11:35 -0300

    feat(sign-up): reuse AuthForm on sign-up page

--- src/pages/Register/RegisterForm.tsx
+++ src/pages/Register/RegisterForm.tsx
@@ -1,9 +1,8 @@
 import { useForm, SubmitHandler, FormProvider } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { DotWave } from "@uiball/loaders";
 import useSignUp from "../../hooks/authHooks/useSignUp";
 import { registerFormSchema } from "../../schemas/credentialsSchemas";
-import PasswordInput from "../../components/PasswordInput";
+import { AuthForm } from "../../components/AuthForm";
 
 type Inputs = {
     name: string;
@@ -19,7 +18,6 @@ export default function RegisterForm() {
     });
 
     const {
-        register,
         handleSubmit,
         setError,
         formState: { errors },
@@ -46,61 +44,54 @@ export default function RegisterForm() {
                 className="flex flex-col gap-3"
                 onSubmit={handleSubmit(onSubmit)}
             >
-                <div className="flex flex-col gap-1">
-                    <label htmlFor="name">Nome completo</label>
-                    <input
-                        id="name"
+                <AuthForm.InputWrapper>
+                    <AuthForm.Label htmlFor="name">
+                        Nome completo
+                    </AuthForm.Label>
+                    <AuthForm.Input
+                        name="name"
                         type="text"
-                        {...register("name")}
                         aria-invalid={errors.name ? "true" : "false"}
                         disabled={isLoading}
                     />
                     {errors.name && (
-                        <p role="alert" className="text-xs text-red-700">
-                            {errors.name.message}
-                        </p>
+                        <AuthForm.ErrorMessage message={errors.name.message} />
                     )}
-                </div>
-                <div className="flex flex-col gap-1">
-                    <label htmlFor="email">E-mail</label>
-                    <input
-                        id="email"
+                </AuthForm.InputWrapper>
+
+                <AuthForm.InputWrapper>
+                    <AuthForm.Label htmlFor="email">E-mail</AuthForm.Label>
+                    <AuthForm.Input
+                        name="email"
                         type="text"
-                        {...register("email")}
                         aria-invalid={errors.email ? "true" : "false"}
                         disabled={isLoading}
                     />
                     {errors.email && (
-                        <p role="alert" className="text-xs text-red-700">
-                            {errors.email.message}
-                        </p>
+                        <AuthForm.ErrorMessage message={errors.email.message} />
                     )}
-                </div>
-                <div className="flex flex-col gap-1">
-                    <label htmlFor="password">Senha</label>
-                    <PasswordInput
+                </AuthForm.InputWrapper>
+
+                <AuthForm.InputWrapper>
+                    <AuthForm.Label htmlFor="password">Senha</AuthForm.Label>
+                    <AuthForm.PasswordInput
                         aria-invalid={errors.password ? "true" : "false"}
                         disabled={isLoading}
                     />
-
                     {errors.password && (
-                        <p role="alert" className="text-xs text-red-700">
-                            {errors.password.message}
-                        </p>
+                        <AuthForm.ErrorMessage
+                            message={errors.password.message}
+                        />
                     )}
-                </div>
+                </AuthForm.InputWrapper>
 
-                <button
-                    type="submit"
-                    className="p-2 bg-pink-500 flex items-center justify-center"
-                    disabled={isLoading}
-                >
-                    {isLoading ? <DotWave /> : "Cadastrar"}
-                </button>
+                <AuthForm.SubmitButton disabled={isLoading}>
+                    Cadastrar
+                </AuthForm.SubmitButton>
                 {errors.root && (
-                    <p role="alert" className="text-xs text-red-700">
-                        {errors.root.serverError.message}
-                    </p>
+                    <AuthForm.ErrorMessage
+                        message={errors.root.serverError.message}
+                    />
                 )}
             </form>
         </FormProvider>
