9492ca2be637aa17821dd038b2ea1594b33fe5f0
augustolfp <augustolfp@gmail.com>
2023-07-15 00:30:46 -0300

    refactor: remove unused components

--- src/components/Input/index.tsx
+++
@@ -1,9 +0,0 @@
-import InputRoot from "./InputRoot";
-import InputRightActions from "./InputRightActions";
-import InputRightAction from "./InputRightAction";
-
-export const Input = {
-    Root: InputRoot,
-    RightActions: InputRightActions,
-    RightAction: InputRightAction,
-};
