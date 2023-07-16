332c877cd4db707b4c2238378aa36c72029a54f5
augustolfp <augustolfp@gmail.com>
2023-07-16 18:23:37 -0300

    refactor(sign-out): use react-firebase-hooks to handle sign-out

--- src/hooks/authHooks/useSignOut.ts
+++ src/hooks/authHooks/useSignOut.ts
@@ -1,27 +1,11 @@
-import { useState } from "react";
-import { signOut as firebaseSignOut } from "firebase/auth";
+import { useSignOut as firebaseSignOut } from "react-firebase-hooks/auth";
 import { auth } from "../../config/firebase";
-import { useAppDispatch } from "../typedReduxHooks";
-import { clearUserData } from "../../store";
 
 export default function useSignOut() {
-    const dispatch = useAppDispatch();
-    const [isLoading, setIsLoading] = useState(false);
-
-    const signOut = async () => {
-        setIsLoading(true);
-        try {
-            await firebaseSignOut(auth);
-            dispatch(clearUserData());
-        } catch (err) {
-            console.log(err);
-        } finally {
-            setIsLoading(false);
-        }
-    };
+    const [signOut, loading, error] = firebaseSignOut(auth);
 
     return {
-        isLoading,
+        isLoading: loading,
         signOut,
     };
 }
