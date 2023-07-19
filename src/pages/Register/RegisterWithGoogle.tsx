a61964984e3d51578a36ee82d18e10e0d93c908e
augustolfp <augustolfp@gmail.com>
2023-07-19 19:26:58 -0300

    fix(google Auth): register with google uses async thunk

--- src/pages/Register/RegisterWithGoogle.tsx
+++ src/pages/Register/RegisterWithGoogle.tsx
@@ -1,18 +1,20 @@
 import { SyntheticEvent } from "react";
-import useGoogleAuth from "../../hooks/authHooks/useGoogleAuth";
 import { AuthForm } from "../../components/AuthForm";
+import { useAppDispatch, useAppSelector } from "../../hooks/typedReduxHooks";
+import { googleAuth } from "../../store/slices/userSlice";
 
 export default function RegisterWithGoogle() {
-    const { isLoading, googleAuth } = useGoogleAuth();
+    const dispatch = useAppDispatch();
+    const { status, error } = useAppSelector((state) => state.user.user);
 
     const handleGoogleRegister = async (e: SyntheticEvent) => {
         e.preventDefault();
-        await googleAuth();
+        dispatch(googleAuth());
     };
 
     return (
         <AuthForm.GoogleButton
-            disabled={isLoading}
+            disabled={status === "loading"}
             onClick={handleGoogleRegister}
         >
             Cadastre-se com o Google
