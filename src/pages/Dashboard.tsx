581e1d796dc13d0befae708d772f12a8358d20c4
augustolfp <augustolfp@gmail.com>
2023-07-19 20:21:04 -0300

    feat(dashboard): implement counted days fetching

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,7 +1,13 @@
+import { useFetchCountedDaysQuery } from "../store";
+import { useAppSelector } from "../hooks/typedReduxHooks";
+
 export default function Dashboard() {
+    const { accessToken } = useAppSelector((state) => state.user.user);
+    const { data, error, isFetching } = useFetchCountedDaysQuery(accessToken!);
     return (
         <div className="flex flex-col items-center">
             <h2>Bem vindo a Dashboard!</h2>
+            <div>{JSON.stringify(data)}</div>
         </div>
     );
 }
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,21 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { countedDaysApi } from "./apis/countedDaysApi";
 import { setUser, clearUserData, userReducer } from "./slices/userSlice";
 
 const store = configureStore({
     reducer: {
         user: userReducer,
+        [countedDaysApi.reducerPath]: countedDaysApi.reducer,
+    },
+    middleware: (getDefaultMiddleware) => {
+        return getDefaultMiddleware().concat(countedDaysApi.middleware);
     },
 });
 
+setupListeners(store.dispatch);
+
+export { useFetchCountedDaysQuery } from "./apis/countedDaysApi";
 export { store, setUser, clearUserData };
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
