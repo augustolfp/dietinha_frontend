865a10d6b96664edee3b550cfccadc941e314a8c
augustolfp <augustolfp@gmail.com>
2023-07-19 19:05:11 -0300

    fix(register form): update form to use redux store info

--- src/store/slices/userSlice.ts
+++ src/store/slices/userSlice.ts
@@ -2,16 +2,22 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
 
 import { auth } from "../../config/firebase";
-import { signInWithEmailAndPassword, signOut } from "firebase/auth";
+import {
+    signInWithEmailAndPassword,
+    createUserWithEmailAndPassword,
+    signOut,
+    updateProfile,
+} from "firebase/auth";
 
 interface SignInCredentials {
     email: string;
     password: string;
 }
 
-interface LoginResponse {
+interface SignUpCredentials {
     displayName: string;
-    accessToken: string;
+    email: string;
+    password: string;
 }
 
 interface User {
@@ -39,6 +45,25 @@ export const signInUser = createAsyncThunk(
         const accessToken = await response.user.getIdToken();
         return {
             displayName: response.user.displayName,
+            accessToken: accessToken,
+        };
+    }
+);
+
+export const signUpUser = createAsyncThunk(
+    "user/signUpUser",
+    async ({ displayName, email, password }: SignUpCredentials) => {
+        const response = await createUserWithEmailAndPassword(
+            auth,
+            email,
+            password
+        );
+        await updateProfile(response.user, {
+            displayName: displayName,
+        });
+        const accessToken = await response.user.getIdToken();
+        return {
+            displayName: displayName,
             accessToken: accessToken,
         };
     }
@@ -83,6 +108,19 @@ const userSlice = createSlice({
                 state.user.status = "loading";
             })
             .addCase(signOutUser.rejected, (state, action) => {
+                state.user.status = "failed";
+                state.user.error = action.error.message;
+            })
+            .addCase(signUpUser.pending, (state, action) => {
+                state.user.status = "loading";
+            })
+            .addCase(signUpUser.fulfilled, (state, action: any) => {
+                state.user.status = "suceeded";
+
+                state.user.displayName = action.payload.displayName;
+                state.user.accessToken = action.payload.accessToken;
+            })
+            .addCase(signUpUser.rejected, (state, action) => {
                 state.user.status = "failed";
                 state.user.error = action.error.message;
             });
