83c954f02747a9e0ef548d956523c2791d140c3b
augustolfp <augustolfp@gmail.com>
2023-09-24 13:15:52 -0300

    refactor(credentials page): move Hero text to component

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,22 +1,14 @@
 import { Link } from "react-router-dom";
 import LoginWithGoogle from "./LoginWithGoogle";
 import LoginForm from "./LoginForm";
+import HeroText from "../../components/HeroText";
 
 export default function Login() {
     return (
         <div className="container mx-auto">
             <div className="hero min-h-screen">
                 <div className="hero-content flex-col lg:flex-row">
-                    <div className="text-center lg:text-left">
-                        <h1 className="text-5xl font-bold">
-                            Conte suas Calorias, Sem Complicações
-                        </h1>
-                        <p className="py-6">
-                            O aplicativo Dietinha oferece uma interface simples,
-                            sem anúncios, e uma lista de alimentos pré-definidos
-                            bem recheada. E o melhor: 100% grátis!
-                        </p>
-                    </div>
+                    <HeroText />
                     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                         <div className="card-body">
                             <h2 className="text-center font-bold">Login</h2>
