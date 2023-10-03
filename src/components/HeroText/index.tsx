31e084d0697c6ce592d553a777fe8471ada3f9a9
augustolfp <augustolfp@gmail.com>
2023-10-03 15:49:55 -0300

    feat(sign-in/sign-up): adjust Hero text size

--- src/components/HeroText/index.tsx
+++ src/components/HeroText/index.tsx
@@ -1,10 +1,10 @@
 export default function HeroText() {
     return (
         <div className="text-left">
-            <h1 className="text-4xl font-medium mb-6">
+            <h1 className="text-4xl font-medium mb-6 lg:text-5xl xl:text-6xl">
                 Conte suas Calorias, Sem Complicações
             </h1>
-            <p className="text-base">
+            <p className="text-base md:text-lg">
                 O aplicativo Dietinha oferece uma interface simples, sem
                 anúncios, e uma lista de alimentos pré-definidos bem recheada. E
                 o melhor: 100% grátis!
