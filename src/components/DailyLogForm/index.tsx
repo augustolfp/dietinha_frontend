96b8b2e546d129d628319668b84ca96230cd4a7c
Augusto <augustolfp@gmail.com>
2023-10-14 18:39:17 -0300

    refactor: move date parsing to Zod schema

--- src/components/DailyLogForm/index.tsx
+++ src/components/DailyLogForm/index.tsx
@@ -1,4 +1,4 @@
-import { Controller, useForm } from "react-hook-form";
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useAddDailyLogMutation } from "../../store/api/apiSlice";
 import {
@@ -6,7 +6,7 @@ import {
     type DailyLogSchema,
 } from "../../schemas/dailyLogSchemas";
 import getApiErrorMessage from "../../services/getApiErrorMessage";
-import { format, parse } from "date-fns";
+import { format } from "date-fns";
 import Calendar from "../Calendar";
 import ProteinsInput from "./ProteinsInput";
 
@@ -24,16 +24,13 @@ export default function DailyLogForm() {
         resolver: zodResolver(dailyLogSchema),
     });
 
-    const onSubmit = async (data: DailyLogSchema) => {
+    const onSubmit: SubmitHandler<DailyLogSchema> = async (data) => {
         try {
             await addDailyLog({
                 notes: data.notes,
-                date: format(
-                    parse(data.date, "dd/MM/yyyy", new Date()),
-                    "yyyy-MM-dd"
-                ),
-                caloriesTarget: Number(data.caloriesTarget),
-                proteinsTarget: Number(data.proteinsTarget),
+                date: data.date,
+                caloriesTarget: data.proteinsTarget,
+                proteinsTarget: data.caloriesTarget,
             }).unwrap();
         } catch (err) {
             const errMessage = getApiErrorMessage(err);
@@ -55,7 +52,7 @@ export default function DailyLogForm() {
                 control={control}
                 name="date"
                 defaultValue={format(new Date(), "dd/MM/yyyy")}
-                render={({ field: { onChange, onBlur, value, ref } }) => (
+                render={({ field: { onChange, value } }) => (
                     <div className="flex flex-col items-center">
                         <Calendar onChange={onChange} value={value} />
                         {errors.date && (
@@ -74,41 +71,6 @@ export default function DailyLogForm() {
                         <ProteinsInput onChange={onChange} value={value} />
                     )}
                 />
-                {/* <div className="flex flex-col gap-3">
-                    <div className="flex justify-between">
-                        <label className="label">
-                            <span className="label-text">
-                                Alvo de proteínas
-                            </span>
-                        </label>
-                        <div className="join">
-                            <input
-                                {...register("proteinsTarget")}
-                                type="number"
-                                placeholder="150"
-                                aria-invalid={
-                                    errors.proteinsTarget ? "true" : "false"
-                                }
-                                className="input input-bordered join-item w-20"
-                            />
-                            <div className="join-item bg-base-300 px-4 text-sm text-base-content flex items-center">
-                                g
-                            </div>
-                        </div>
-                    </div>
-                    <input
-                        type="range"
-                        min={0}
-                        max="100"
-                        value="40"
-                        readOnly={true}
-                        className="range range-secondary range-sm"
-                    />
-                    {errors.proteinsTarget && (
-                        <p className="text-red-500">{`${errors.proteinsTarget.message}`}</p>
-                    )}
-                </div> */}
-
                 <div className="divider divider-vertical"></div>
 
                 <div className="flex flex-col gap-3">
--- src/schemas/dailyLogSchemas.ts
+++ src/schemas/dailyLogSchemas.ts
@@ -1,7 +1,10 @@
 import { z } from "zod";
+import {format, parse} from "date-fns"
 
 export const dailyLogSchema = z.object({
-    date: z.string(),
+    date: z.preprocess((dateStr) => {
+        return parse(String(dateStr), "dd/MM/yyyy", new Date())
+    }, z.date().transform((date) => format(date, "yyyy-MM-dd"))),
     notes: z.string().optional(),
     caloriesTarget: z.coerce
         .number()
