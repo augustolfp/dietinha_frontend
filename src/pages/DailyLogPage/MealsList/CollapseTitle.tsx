3f9ff59c510d5e905210ddb247456c42ee37b156
Augusto <augustolfp@gmail.com>
2023-10-19 18:29:49 -0300

    refactor: move Meal Collapse title into separate component

---
+++ src/pages/DailyLogPage/MealsList/CollapseTitle.tsx
@@ -0,0 +1,40 @@
+import useUser from "../../../hooks/authHooks/useUser";
+import { useGetMealSummaryQuery } from "../../../store/api/apiSlice";
+import MealStats from "../../../components/MealStats";
+
+interface Props {
+    mealId: string;
+}
+
+export default function CollapseTitle({ mealId }: Props) {
+    const { accessToken } = useUser();
+    const { data, error, isLoading } = useGetMealSummaryQuery(
+        { id: mealId },
+        { skip: !Boolean(accessToken) }
+    );
+
+    let mealResume;
+    if (isLoading) {
+        mealResume = <p>Loading...</p>;
+    } else if (error) {
+        mealResume = <p className="text-red-600">Error on fetching</p>;
+    } else if (data) {
+        mealResume = (
+            <div className="flex flex-col sm:flex-row sm:justify-between">
+                <h3 className="font-medium text-neutral">{data.name}</h3>
+                <MealStats
+                    kcals={data.kcals}
+                    carbs={data.carbs}
+                    proteins={data.proteins}
+                    fats={data.fats}
+                />
+            </div>
+        );
+    }
+
+    return (
+        <summary className="collapse-title bg-[#DCE1FF] rounded-xl">
+            {mealResume}
+        </summary>
+    );
+}
--- src/pages/DailyLogPage/MealsList/MealsListItem.tsx
+++ src/pages/DailyLogPage/MealsList/MealsListItem.tsx
@@ -5,6 +5,7 @@ import { useMediaQuery } from "../../../hooks/useMediaQuery";
 import IngredientsList from "../../../components/IngredientsList";
 import Drawer from "../../../components/Drawer";
 import AddIngredientTab from "../../../components/AddIngredientTab";
+import CollapseTitle from "./CollapseTitle";
 
 interface Props {
     mealId: string;
@@ -12,36 +13,10 @@ interface Props {
 
 export default function MealsListItem({ mealId }: Props) {
     const isDesktop = useMediaQuery("(min-width: 768px)");
-    const { accessToken } = useUser();
-    const { data, error, isLoading } = useGetMealSummaryQuery(
-        { id: mealId },
-        { skip: !Boolean(accessToken) }
-    );
-
-    let content;
-    if (isLoading) {
-        content = <p>Loading...</p>;
-    } else if (error) {
-        content = <p className="text-red-600">Error on fetching</p>;
-    } else if (data) {
-        content = (
-            <div className="flex flex-col sm:flex-row sm:justify-between">
-                <h3 className="font-medium text-neutral">{data.name}</h3>
-                <MealStats
-                    kcals={data.kcals}
-                    carbs={data.carbs}
-                    proteins={data.proteins}
-                    fats={data.fats}
-                />
-            </div>
-        );
-    }
 
     return (
         <details className="collapse collapse-arrow">
-            <summary className="collapse-title bg-[#DCE1FF] rounded-xl">
-                {content}
-            </summary>
+            <CollapseTitle mealId={mealId} />
             <div className="collapse-content">
                 <div className="h-72">
                     <div className="md:flex md: justify-between">
