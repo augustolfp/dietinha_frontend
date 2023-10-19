6025782fa49d4a6eb999adf0b4a9a7dacf97f9b9
Augusto <augustolfp@gmail.com>
2023-10-19 19:45:46 -0300

    refactor: move drawer and footer into MobileInterface directory

--- src/components/AddIngredientsInterface/MobileInterface/index.tsx
+++ src/components/AddIngredientsInterface/MobileInterface/index.tsx
@@ -1,18 +1,33 @@
-import Drawer from "../../Drawer";
+import { useState } from "react";
+import Drawer from "./Drawer";
 import AddIngredientTab from "../../AddIngredientTab";
+import Footer from "./Footer";
 
 interface Props {
     mealId: string;
 }
 
 export default function MobileInterface({ mealId }: Props) {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
-        <div className="flex justify-between items-center">
-            <h2>Ingredientes</h2>
-            <Drawer mealId={mealId}>
-                <p>Mobile form</p>
-                <AddIngredientTab mealId={mealId} />
-            </Drawer>
+        <div>
+            <div className="flex justify-between items-center">
+                <h2>Ingredientes</h2>
+                <button
+                    className="btn btn-neutral btn-md aspect-square"
+                    onClick={() => setIsOpen(true)}
+                    disabled={isOpen}
+                >
+                    +
+                </button>
+            </div>
+            {isOpen && (
+                <Drawer onClose={() => setIsOpen(false)}>
+                    <AddIngredientTab mealId={mealId} />
+                    <Footer mealId={mealId} />
+                </Drawer>
+            )}
         </div>
     );
 }
